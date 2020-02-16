407ad8c0efbfde1a529779bc3bd163db56ba6b7b
Jessica Hotsko <jhotsko@tulane.edu>
2020-02-16 20:29:05 +0000

    Change gmail smtp port number back to 587

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -69,7 +69,7 @@ Rails.application.configure do
   # SMTP settings for gmail
   config.action_mailer.smtp_settings = {
     :address              => "smtp.gmail.com",
-    :port                 => 465,
+    :port                 => 587,
     :user_name            => ENV["GMAIL_USERNAME"],
     :password             => ENV["GMAIL_PASSWORD"],
     :authentication       => "plain",
