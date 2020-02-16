f02b1dc233d61ae9657cf5b104f1d2171761ec2d
Jessica Hotsko <jhotsko@tulane.edu>
2020-02-16 04:39:49 +0000

    Update Url

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -81,7 +81,7 @@ Rails.application.configure do
   # config.logger = ActiveSupport::TaggedLogging.new(Syslog::Logger.new 'app-name')
   
   config.action_mailer.delivery_method = :smtp
-  host = 'https://rotsee2.herokuapp.com/' #replace with your own url
+  host = 'https://afrotsee.com/' #replace with your own url
   config.action_mailer.default_url_options = { host: host }
 
   # SMTP settings for gmail
